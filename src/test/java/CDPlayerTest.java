58b60fb89ee5977d1e1732cac6c743fb07d444d2
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-04 13:12:16 +0000

    fixes cdplayer var

--- src/test/java/CDPlayerTest.java
+++ src/test/java/CDPlayerTest.java
@@ -6,61 +6,61 @@ import static org.junit.Assert.assertEquals;
 
 public class CDPlayerTest {
 
-    private CDPlayer CDPlayer1;
+    private CDPlayer cdPlayer1;
 
     @Before
     public void before() {
-        this.CDPlayer1 = new CDPlayer("JVC", "BR0K3");
+        this.cdPlayer1 = new CDPlayer("JVC", "BR0K3");
     }
 
     @Test
     public void canGetMake() {
-        assertEquals("JVC", CDPlayer1.getMake());
+        assertEquals("JVC", cdPlayer1.getMake());
     }
 
     @Test
     public void canGetModel() {
-        assertEquals("BR0K3", CDPlayer1.getModel());
+        assertEquals("BR0K3", cdPlayer1.getModel());
     }
 
     @Test
     public void canSetTape() {
-        CDPlayer1.setCD("The Wall");
-        assertEquals("The Wall", CDPlayer1.getCD());
+        cdPlayer1.setCD("The Wall");
+        assertEquals("The Wall", cdPlayer1.getCD());
     }
 
     @Test
     public void canPlay() {
-        CDPlayer1.setCD("The Wall");
-        assertEquals("The Wall is currently playing track 1", CDPlayer1.play());
+        cdPlayer1.setCD("The Wall");
+        assertEquals("The Wall is currently playing track 1", cdPlayer1.play());
     }
 
     @Test
     public void canPause() {
-        CDPlayer1.setCD("The Wall");
-        assertEquals("The Wall has been paused!", CDPlayer1.pause());
+        cdPlayer1.setCD("The Wall");
+        assertEquals("The Wall has been paused!", cdPlayer1.pause());
     }
 
     @Test
     public void canStop() {
-        CDPlayer1.setCD("The Wall");
-        assertEquals("The Wall has now stopped!", CDPlayer1.stop());
+        cdPlayer1.setCD("The Wall");
+        assertEquals("The Wall has now stopped!", cdPlayer1.stop());
     }
 
     @Test
     public void canSkipTrack(){
-        CDPlayer1.setCD("The Wall");
-        CDPlayer1.skipTrack();
-        assertEquals("The Wall is currently playing track 2", CDPlayer1.play());
+        cdPlayer1.setCD("The Wall");
+        cdPlayer1.skipTrack();
+        assertEquals("The Wall is currently playing track 2", cdPlayer1.play());
     }
 
     @Test
     public void canTurnOn(){
-        assertEquals("This is on" , CDPlayer1.on());
+        assertEquals("This is on" , cdPlayer1.on());
     }
     @Test
     public void canTurnOff(){
-        assertEquals("This is off", CDPlayer1.off());
+        assertEquals("This is off", cdPlayer1.off());
     }
 
 
